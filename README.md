e8f7f0aa5e860611a9affdac0773b68e96991776
LorenBen <fengluojun2022@163.com>
2026-02-26 01:25:27 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Loren_Projects
 个人项目
+
+# 本地下拉指定模块
+git submodule update --init -- TenniBot/Tennibot_code
--- TenniBot/resource/创客创业计划/程序/emphasizeLaneFeatures.m
+++
@@ -1,7 +0,0 @@
-function L = emphasizeLaneFeatures(I, tau)
-    Ipad = padarray(I, [0 double(tau)+1], 'replicate');
-    Ileft  = single(Ipad(:,1:end-2*(tau+1)));
-    Iright = single(Ipad(:,1+2*(tau+1):end));
-    L = 2*single(I) - (Ileft+Iright) - (abs(Ileft - Iright));
-    L = imnormalize(L);
-end
